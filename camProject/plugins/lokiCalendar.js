d836bd6688219cec10006c8edec39fd49fd313f9
Ashlee <mee60407@gmail.com>
2024-11-30 22:28:53 +0800

    1130

--- camProject/plugins/lokiCalendar.js
+++ camProject/plugins/lokiCalendar.js
@@ -1,6 +1,4 @@
-fetch()//串接資料
 dayjs.locale('zh-tw');
-
 
 //宣告全域變數區
 let
@@ -39,12 +37,13 @@ const runCalendarService = () => {
       thisDate: theDay.add(1, 'month'),
     },
 
-    listMaker = (obj) => {//調整萬年曆物件，調整完畢後，返回修改後的物件
+    listMaker = (obj) => {//調整萬年曆物件，調整完畢後，返回修改後的物件//❓為何這個fn中的obj會知道是calLeft或calRight
       // const firstDay = obj.thisDate.date(1).day();
-      const firstDay = obj.thisDate.startOf('month').day();//該月第一天禮拜幾
+      const firstDay = obj.thisDate.startOf('month').day();//該月第一天為禮拜幾
       const totalDay = obj.thisDate.daysInMonth();//該月有幾天
       // console.log(firstDay, totalDay);
 
+      //1 = mon, 2 = tue, 3 = wed, 4 = thu, 5 = fri, 6 = sat, 0 = sun
       for (let i = 1; i < (firstDay || 7); i++) {//控制產生多少空白日
         obj.listBox += `<li calss="JsCal"></li>`;
       }
@@ -52,26 +51,32 @@ const runCalendarService = () => {
       for (let i = 1; i <= totalDay; i++) {//控制產生多少日期
         obj.listBox += `<li calss="JsCal">${i}</li>`;
       }
-      //[方法一]//這邊還沒寫完
-      // obj.title=`monthTostring[obj.thisDate.month()] ${obj.thisDate.year()}`;
-      //[方法二]
-      const monthTostring = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-      obj.title = `${monthTostring[obj.thisDate.month()]} ${obj.thisDate.year()}`;
-      //[方法三]
-      const twMonth=window.dayjs_locale_zh_tw.months;
+
+      //將月曆的標題代換為目前的月份
+      //[方法一]
+      // obj.title = `${obj.thisDate.month()+1}月 ${obj.thisDate.year()}年`;
+
+      // [方法二]
+      // const monthTostring = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      // obj.title = `${monthTostring[obj.thisDate.month()]} ${obj.thisDate.year()}`;
+
+      // // [方法三]
+      const twMonth = window.dayjs_locale_zh_tw.months;
       obj.title = `${twMonth[obj.thisDate.month()]} ${obj.thisDate.year()}`;
 
       return obj;
     },
 
     listPrint = () => {//準備輸出到DOM
-      // console.log(listMaker(calLeft).listBox);
-      document.querySelector('.leftDaylist').innerHTML = listMaker(calLeft).listBox;
-      document.querySelector('.rightDaylist').innerHTML = listMaker(calRight).listBox;
+      // console.log(listMaker(calLeft));
+      document.querySelector('.leftDayList').innerHTML = listMaker(calLeft).listBox;
+      document.querySelector('.rightDayList').innerHTML = listMaker(calRight).listBox;
 
       document.querySelector('.leftBar>h4').innerHTML = listMaker(calLeft).title;
       document.querySelector('.rightBar>h4').innerHTML = listMaker(calRight).title;
+
     }
+  listPrint();
 };
 
 init();
